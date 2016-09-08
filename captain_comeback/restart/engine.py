d429500e0838b0325c88297c4211309d33431080
Thomas Orozco <thomas@orozco.fr>
2016-09-08 20:46:10 +0200

    Workaround Docker's "container destroyed"
    When a container exists right around when Docker was attempting to
    restart it, it might get into an awkward state that makes it impossible
    to restart. Resolving the issue involves restarting the Docker daemon,
    which isn't particularly desirable for us.
    
    So, in order to be able to send SIGTERM to all processes in a cgroup,
    without making Docker crazy, we'll just wait for the cgroup to exit
    ourselves. If it doesn't, then we'll ask Docker to kill it with
    prejudice immediately (i.e. `docker kill -t 0`).
    
    This appears to be resolved in 1.11 (and to be most common on 1.9), but
    not all our infrastructure is there yet.
    
    See: https://github.com/docker/docker/issues/12738

--- captain_comeback/restart/engine.py
+++ captain_comeback/restart/engine.py
@@ -4,6 +4,7 @@ import signal
 import logging
 import threading
 import subprocess
+import time
 
 import psutil
 
@@ -13,6 +14,9 @@ from captain_comeback.activity.messages import RestartCgroupMessage
 
 
 logger = logging.getLogger()
+
+
+RESTART_STATE_POLLS = 20
 
 
 class RestartEngine(object):
@@ -75,34 +79,58 @@ def restart(grace_period, cg, job_queue, activity_queue):
             # That process exited already. Who cares? We don't.
             logger.debug("%s: %s had already exited", cg.name(), pid)
 
-    restart_cmd = ["docker", "restart", "-t", str(grace_period), cg.name()]
-    proc = subprocess.Popen(restart_cmd, stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
+    signaled_at = time.time()
 
-    # Try and allocate 10% of extra memory to give this cgroup a chance to
-    # shut down gracefully.
-    # NOTE: we look at free memory (rather than available) so that we don't
-    # have to e.g. free some buffers to grant this extra memory.
-    memory_limit = cg.memory_limit_in_bytes()
-    free_memory = psutil.virtual_memory().free
-    extra = int(memory_limit / 10)  # Make parameterizable
+    # All that follows is optimistic. In practice, the container could
+    # exit immediately and none of that would get a chance to run.
 
-    logger.debug("%s: memory_limit: %s, free_memory: %s, extra: %s",
-                 cg.name(), memory_limit, free_memory, extra)
-    if free_memory > extra:
-        new_limit = memory_limit + extra
-        logger.info("%s: increasing memory limit to %s", cg.name(),
-                    new_limit)
-        cg.set_memory_limit_in_bytes(new_limit)
+    try:
+        # Try and allocate 10% of extra memory to give this cgroup a chance to
+        # shut down gracefully. Note that we look at free memory (rather than
+        # available) so that we don't have to e.g. free some buffers to grant
+        # this extra memory.
+        memory_limit = cg.memory_limit_in_bytes()
+        free_memory = psutil.virtual_memory().free
+        extra = int(memory_limit / 10)  # Make parameterizable
 
-    out, err = proc.communicate()
-    ret = proc.poll()
-    if ret != 0:
-        logger.error("%s: failed to restart", cg.name())
-        logger.error("%s: status: %s", cg.name(), ret)
-        logger.error("%s: stdout: %s", cg.name(), out)
-        logger.error("%s: stderr: %s", cg.name(), err)
+        logger.debug("%s: memory_limit: %s, free_memory: %s, extra: %s",
+                     cg.name(), memory_limit, free_memory, extra)
+        if free_memory > extra:
+            new_limit = memory_limit + extra
+            logger.info("%s: increasing memory limit to %s", cg.name(),
+                        new_limit)
+            cg.set_memory_limit_in_bytes(new_limit)
 
-    # TODO: Make this a finally?
-    logger.info("%s: restart complete", cg.name())
-    job_queue.put(RestartCompleteMessage(cg))
+        # Now, we give grace_period to the container to exit. We'd like to use
+        # docker restart -t for this, but unfortunately that does not work
+        # reliably: https://github.com/docker/docker/issues/12738
+        while time.time() < signaled_at + grace_period:
+            time.sleep(float(grace_period) / RESTART_STATE_POLLS)
+            try:
+                pids = cg.pids()
+            except EnvironmentError:
+                # The cgroup is gone!
+                logger.debug("%s: cgroup has exited after SIGTERM ", cg.name())
+                break
+            else:
+                logger.debug("%s: Waiting for processes to exit: %s...",
+                             cg.name(), ", ".join(str(p) for p in pids))
+    except EnvironmentError:
+        # This could happen if e.g. attempting to write to the memory limit
+        # file after the cgroup has exited.
+        pass
+    finally:
+        restart_cmd = ["docker", "restart", "-t", "0", cg.name()]
+        proc = subprocess.Popen(restart_cmd, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+
+        out, err = proc.communicate()
+        ret = proc.poll()
+        if ret != 0:
+            logger.error("%s: failed to restart", cg.name())
+            logger.error("%s: status: %s", cg.name(), ret)
+            logger.error("%s: stdout: %s", cg.name(), out)
+            logger.error("%s: stderr: %s", cg.name(), err)
+
+        logger.info("%s: restart complete", cg.name())
+        job_queue.put(RestartCompleteMessage(cg))
